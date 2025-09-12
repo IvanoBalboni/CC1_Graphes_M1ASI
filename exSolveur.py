f8efc045ddfc275c4ca06d14e02610fcfdbdb1a2
Albert Peltier <albert.peltier@etu.unicaen.fr>
2025-09-12 12:06:07 +0200

    ex7

--- exSolveur.py
+++ exSolveur.py
@@ -19,13 +19,13 @@ def uneSolution(f):
     else:
         print("la formule n'est pas satisfaisable")
 
-    
+
 
 if __name__=='__main__':
+    """
     print("\nExemple 1")
     clauses = [(1, 2), (-1, 2, -3), (1, -2, -3), (-1, -2, 3), (-1, -2, -3)]
-    #clauses_a = [(1), (2, -1, -3), (2, 3, 4), (-2, -1, -3), (-1, 3, -4), (-1, 3, -4), (-3, 4)]
-    #clauses_b = 
+    
 
     formule1 = CNF(from_clauses=clauses)
     print('\n-- la formule\n%s\n' %formule1.to_dimacs())
@@ -39,7 +39,25 @@ if __name__=='__main__':
     print('\n-- la formule\n%s\n' %formule2.to_dimacs())
 
     toutesLesSolutions(formule2)
+    """
     
+    
+    clauses_a = [(1,), (2, -1, -3), (2, 3, 4), (-2, -1, -3), (-1, 3, -4), (-3, 4)]
+    clauses_b = [(-1, -2), (3, -4), (1, 2, 3), (1, 2, -3, 4), (1, -2, 3), (1, -3), (-1, -3, 4)]
 
+    formule_a = CNF(from_clauses=clauses_a)
+    formule_b = CNF(from_clauses=clauses_b)
+    
+    print('\n-- la formule a\n%s\n' %formule_a.to_dimacs())
+    toutesLesSolutions(formule_a)
+    uneSolution(formule_a)
+    
+    print('\n-- la formule b\n%s\n' %formule_b.to_dimacs())
+    toutesLesSolutions(formule_b)
+    uneSolution(formule_b)
+
+    
+    
+    
 
 
